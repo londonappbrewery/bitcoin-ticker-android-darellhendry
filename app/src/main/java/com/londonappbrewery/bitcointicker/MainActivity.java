3ec164b3d4b725893284001c115ae3677f5e1b41
darellhendry <1darellhendry@gmail.com>
2018-08-23 16:02:40 +0700

    done!!

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,17 +12,24 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URI;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
-
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/";
+    private final String TAG_APP = "Bitcoin";
     // Member Variables:
     TextView mPriceTextView;
 
@@ -45,33 +52,56 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String currency = (String) adapterView.getItemAtPosition(i);
+                Log.d(TAG_APP, ""+ currency);
+                String newUrl = BASE_URL + "indices/global/ticker/"+"BTC"+currency;
+                letsDoSomeNetworking(newUrl);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d(TAG_APP, "Nothing selected");
+            }
+        });
 
     }
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(this, url, new JsonHttpResponseHandler() {
 
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                Log.d(TAG_APP, response.toString());
+                updateUI(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d(TAG_APP, "Request fail! Status code: " + statusCode);
+                Log.d(TAG_APP, "Fail response: " + response);
+                Log.e(TAG_APP, e.toString());
+                Toast.makeText(MainActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void updateUI(JSONObject response) {
+        try {
+            int newCurrency = response.getInt("bid");
+            mPriceTextView.setText("" + newCurrency);
+        } catch (JSONException e) {
+            Log.e("Error", e.getMessage());
+        }
 
     }
 
